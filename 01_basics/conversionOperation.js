f70411088b53280d5c284d801707e39b790eb9d7
ketanmangale <40220933+ketanmangale@users.noreply.github.com>
2023-08-18 13:26:46 +0000

    ConversionOfDatatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,14 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber);
+//Conver to number
+// "33" => 33
+//"33afg" => NaN
+//null => 0
+//true => 1
+//false => 0
+//undefined => NaN
