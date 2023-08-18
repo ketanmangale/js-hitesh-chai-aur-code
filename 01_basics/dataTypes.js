45c60215db30129ede53d12a1cd1d442c1c10ea5
ketanmangale <40220933+ketanmangale@users.noreply.github.com>
2023-08-18 13:15:12 +0000

    Datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//alert(3+3) //We are using nodejs not browser
+//number = 1034 
+//bigint = 135435345352
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefiend = not defined 
+
+console.log(typeof null) // Type is Object
+console.log(typeof undefined)      // type is undefined
