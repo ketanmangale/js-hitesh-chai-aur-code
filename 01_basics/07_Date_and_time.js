4b3a8a3c92f6a640ffa8d3adc662e6540a9d08f0
ketanmangale <40220933+ketanmangale@users.noreply.github.com>
2023-08-25 15:09:40 +0000

    Date and time opearations

---
+++ 01_basics/07_Date_and_time.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(typeof myDate)
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toString())
+
+
+//let myCreateDate = new Date(2023,7,23)
+//let myCreateDate = new Date("2023-02-14")
+let myCreateDate = new Date(2023,7,23,5,7,8)
+// console.log(myCreateDate.toDateString())
+// console.log(myCreateDate.toLocaleString())
+
+let newDate = new Date()
+
+console.log(newDate)
+
+console.log(newDate.getFullYear())
+console.log(newDate.getMonth()+ 1)
+console.log(newDate.getDay())
+console.log(newDate.getHours())
+console.log(newDate.getMinutes())
