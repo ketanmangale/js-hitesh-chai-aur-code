79068cd1211ad262e6d85fea5f7c3ae8259e02cf
ketanmangale <40220933+ketanmangale@users.noreply.github.com>
2023-08-25 09:02:58 +0000

    changes in dataTypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Ketan")
+const accountId= 176474
+let accountEmail="ketansmangale"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState
+
+//accountId = 2
+
+//console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
